67be1f7fd8c585fc2d14418161d427b5ce0ee1a9
Matthew Elger <mattelger@icloud.com>
2017-05-11 00:04:37 +0100

    Add files via upload

---
+++ VB/Pupils/Pupils/Pupils/staffDetails.Designer.vb
@@ -0,0 +1,278 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class staffDetails
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.txtEmergencyNo = New System.Windows.Forms.TextBox()
+        Me.txtHomeAdd = New System.Windows.Forms.TextBox()
+        Me.txtSurname = New System.Windows.Forms.TextBox()
+        Me.txtFirstName = New System.Windows.Forms.TextBox()
+        Me.lblEmergencyNo = New System.Windows.Forms.Label()
+        Me.txtStaffID = New System.Windows.Forms.TextBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.lblHomeAddress = New System.Windows.Forms.Label()
+        Me.lblSurname = New System.Windows.Forms.Label()
+        Me.lblFirstName = New System.Windows.Forms.Label()
+        Me.lblStaffID = New System.Windows.Forms.Label()
+        Me.btnRetreive = New System.Windows.Forms.Button()
+        Me.lblDepartment = New System.Windows.Forms.Label()
+        Me.txtDepartment = New System.Windows.Forms.TextBox()
+        Me.dtpDOB = New System.Windows.Forms.DateTimePicker()
+        Me.lblDOB = New System.Windows.Forms.Label()
+        Me.btnBack = New System.Windows.Forms.Button()
+        Me.cmbStaffID = New System.Windows.Forms.ComboBox()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnSave
+        '
+        Me.btnSave.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.btnSave.Location = New System.Drawing.Point(3, 234)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(96, 32)
+        Me.btnSave.TabIndex = 18
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = False
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.lblName.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblName.Location = New System.Drawing.Point(6, 6)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(78, 29)
+        Me.lblName.TabIndex = 9
+        Me.lblName.Text = "   Staff"
+        '
+        'txtEmergencyNo
+        '
+        Me.txtEmergencyNo.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.txtEmergencyNo.Location = New System.Drawing.Point(98, 136)
+        Me.txtEmergencyNo.Name = "txtEmergencyNo"
+        Me.txtEmergencyNo.Size = New System.Drawing.Size(112, 20)
+        Me.txtEmergencyNo.TabIndex = 5
+        '
+        'txtHomeAdd
+        '
+        Me.txtHomeAdd.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.txtHomeAdd.Location = New System.Drawing.Point(98, 112)
+        Me.txtHomeAdd.Name = "txtHomeAdd"
+        Me.txtHomeAdd.Size = New System.Drawing.Size(112, 20)
+        Me.txtHomeAdd.TabIndex = 4
+        '
+        'txtSurname
+        '
+        Me.txtSurname.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.txtSurname.Location = New System.Drawing.Point(98, 88)
+        Me.txtSurname.Name = "txtSurname"
+        Me.txtSurname.Size = New System.Drawing.Size(112, 20)
+        Me.txtSurname.TabIndex = 3
+        '
+        'txtFirstName
+        '
+        Me.txtFirstName.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.txtFirstName.Location = New System.Drawing.Point(98, 64)
+        Me.txtFirstName.Name = "txtFirstName"
+        Me.txtFirstName.Size = New System.Drawing.Size(112, 20)
+        Me.txtFirstName.TabIndex = 2
+        '
+        'lblEmergencyNo
+        '
+        Me.lblEmergencyNo.AutoSize = True
+        Me.lblEmergencyNo.Location = New System.Drawing.Point(8, 136)
+        Me.lblEmergencyNo.Name = "lblEmergencyNo"
+        Me.lblEmergencyNo.Size = New System.Drawing.Size(77, 13)
+        Me.lblEmergencyNo.TabIndex = 14
+        Me.lblEmergencyNo.Text = "Emergency No"
+        '
+        'txtStaffID
+        '
+        Me.txtStaffID.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.txtStaffID.Enabled = False
+        Me.txtStaffID.Location = New System.Drawing.Point(98, 40)
+        Me.txtStaffID.Name = "txtStaffID"
+        Me.txtStaffID.Size = New System.Drawing.Size(112, 20)
+        Me.txtStaffID.TabIndex = 1
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Panel1.Controls.Add(Me.cmbStaffID)
+        Me.Panel1.Controls.Add(Me.btnBack)
+        Me.Panel1.Controls.Add(Me.lblDOB)
+        Me.Panel1.Controls.Add(Me.dtpDOB)
+        Me.Panel1.Controls.Add(Me.txtDepartment)
+        Me.Panel1.Controls.Add(Me.lblDepartment)
+        Me.Panel1.Controls.Add(Me.btnSave)
+        Me.Panel1.Controls.Add(Me.lblName)
+        Me.Panel1.Controls.Add(Me.txtEmergencyNo)
+        Me.Panel1.Controls.Add(Me.txtHomeAdd)
+        Me.Panel1.Controls.Add(Me.txtSurname)
+        Me.Panel1.Controls.Add(Me.txtFirstName)
+        Me.Panel1.Controls.Add(Me.lblEmergencyNo)
+        Me.Panel1.Controls.Add(Me.txtStaffID)
+        Me.Panel1.Controls.Add(Me.lblHomeAddress)
+        Me.Panel1.Controls.Add(Me.lblSurname)
+        Me.Panel1.Controls.Add(Me.lblFirstName)
+        Me.Panel1.Controls.Add(Me.lblStaffID)
+        Me.Panel1.Controls.Add(Me.btnRetreive)
+        Me.Panel1.Location = New System.Drawing.Point(32, 13)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(213, 284)
+        Me.Panel1.TabIndex = 21
+        '
+        'lblHomeAddress
+        '
+        Me.lblHomeAddress.AutoSize = True
+        Me.lblHomeAddress.Location = New System.Drawing.Point(8, 112)
+        Me.lblHomeAddress.Name = "lblHomeAddress"
+        Me.lblHomeAddress.Size = New System.Drawing.Size(76, 13)
+        Me.lblHomeAddress.TabIndex = 13
+        Me.lblHomeAddress.Text = "Home Address"
+        '
+        'lblSurname
+        '
+        Me.lblSurname.AutoSize = True
+        Me.lblSurname.Location = New System.Drawing.Point(8, 88)
+        Me.lblSurname.Name = "lblSurname"
+        Me.lblSurname.Size = New System.Drawing.Size(49, 13)
+        Me.lblSurname.TabIndex = 12
+        Me.lblSurname.Text = "Surname"
+        '
+        'lblFirstName
+        '
+        Me.lblFirstName.AutoSize = True
+        Me.lblFirstName.Location = New System.Drawing.Point(8, 64)
+        Me.lblFirstName.Name = "lblFirstName"
+        Me.lblFirstName.Size = New System.Drawing.Size(57, 13)
+        Me.lblFirstName.TabIndex = 11
+        Me.lblFirstName.Text = "First Name"
+        '
+        'lblStaffID
+        '
+        Me.lblStaffID.AutoSize = True
+        Me.lblStaffID.Location = New System.Drawing.Point(8, 40)
+        Me.lblStaffID.Name = "lblStaffID"
+        Me.lblStaffID.Size = New System.Drawing.Size(40, 13)
+        Me.lblStaffID.TabIndex = 10
+        Me.lblStaffID.Text = "StaffID"
+        '
+        'btnRetreive
+        '
+        Me.btnRetreive.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.btnRetreive.Location = New System.Drawing.Point(114, 234)
+        Me.btnRetreive.Name = "btnRetreive"
+        Me.btnRetreive.Size = New System.Drawing.Size(96, 32)
+        Me.btnRetreive.TabIndex = 0
+        Me.btnRetreive.Text = "Retreive"
+        Me.btnRetreive.UseVisualStyleBackColor = False
+        '
+        'lblDepartment
+        '
+        Me.lblDepartment.AutoSize = True
+        Me.lblDepartment.Location = New System.Drawing.Point(8, 161)
+        Me.lblDepartment.Name = "lblDepartment"
+        Me.lblDepartment.Size = New System.Drawing.Size(62, 13)
+        Me.lblDepartment.TabIndex = 19
+        Me.lblDepartment.Text = "Department"
+        '
+        'txtDepartment
+        '
+        Me.txtDepartment.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.txtDepartment.Location = New System.Drawing.Point(98, 158)
+        Me.txtDepartment.Name = "txtDepartment"
+        Me.txtDepartment.Size = New System.Drawing.Size(112, 20)
+        Me.txtDepartment.TabIndex = 20
+        '
+        'dtpDOB
+        '
+        Me.dtpDOB.Location = New System.Drawing.Point(98, 184)
+        Me.dtpDOB.Name = "dtpDOB"
+        Me.dtpDOB.Size = New System.Drawing.Size(112, 20)
+        Me.dtpDOB.TabIndex = 21
+        '
+        'lblDOB
+        '
+        Me.lblDOB.AutoSize = True
+        Me.lblDOB.Location = New System.Drawing.Point(8, 190)
+        Me.lblDOB.Name = "lblDOB"
+        Me.lblDOB.Size = New System.Drawing.Size(66, 13)
+        Me.lblDOB.TabIndex = 22
+        Me.lblDOB.Text = "Date of Birth"
+        '
+        'btnBack
+        '
+        Me.btnBack.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.btnBack.Location = New System.Drawing.Point(114, 234)
+        Me.btnBack.Name = "btnBack"
+        Me.btnBack.Size = New System.Drawing.Size(96, 32)
+        Me.btnBack.TabIndex = 23
+        Me.btnBack.Text = "Back"
+        Me.btnBack.UseVisualStyleBackColor = False
+        Me.btnBack.Visible = False
+        '
+        'cmbStaffID
+        '
+        Me.cmbStaffID.FormattingEnabled = True
+        Me.cmbStaffID.Location = New System.Drawing.Point(-13, 241)
+        Me.cmbStaffID.Name = "cmbStaffID"
+        Me.cmbStaffID.Size = New System.Drawing.Size(121, 21)
+        Me.cmbStaffID.TabIndex = 24
+        Me.cmbStaffID.Visible = False
+        '
+        'staffDetails
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(276, 310)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "staffDetails"
+        Me.Text = "staffDetails"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnSave As System.Windows.Forms.Button
+    Friend WithEvents lblName As System.Windows.Forms.Label
+    Friend WithEvents txtEmergencyNo As System.Windows.Forms.TextBox
+    Friend WithEvents txtHomeAdd As System.Windows.Forms.TextBox
+    Friend WithEvents txtSurname As System.Windows.Forms.TextBox
+    Friend WithEvents txtFirstName As System.Windows.Forms.TextBox
+    Friend WithEvents lblEmergencyNo As System.Windows.Forms.Label
+    Friend WithEvents txtStaffID As System.Windows.Forms.TextBox
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents lblDOB As System.Windows.Forms.Label
+    Friend WithEvents dtpDOB As System.Windows.Forms.DateTimePicker
+    Friend WithEvents txtDepartment As System.Windows.Forms.TextBox
+    Friend WithEvents lblDepartment As System.Windows.Forms.Label
+    Friend WithEvents lblHomeAddress As System.Windows.Forms.Label
+    Private WithEvents lblSurname As System.Windows.Forms.Label
+    Friend WithEvents lblFirstName As System.Windows.Forms.Label
+    Friend WithEvents lblStaffID As System.Windows.Forms.Label
+    Friend WithEvents btnRetreive As System.Windows.Forms.Button
+    Friend WithEvents cmbStaffID As System.Windows.Forms.ComboBox
+    Friend WithEvents btnBack As System.Windows.Forms.Button
+End Class
---
+++ VB/Pupils/Pupils/Pupils/staffDetails.vb
@@ -0,0 +1,215 @@
+﻿Imports System.IO
+
+Public Class staffDetails
+
+    Private Structure Staff
+        Public StaffId As String
+        Public FirstName As String
+        Public Surname As String
+        Public HomeAdd As String                  'Creating the structure that will hold the  data.
+        Public EmergencyNo As String
+        Public Department As String
+        Public DOB As Date
+    End Structure
+
+
+    Private Sub staffDetails_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        If Dir$("Staff.txt") = "" Then
+            Dim sw As New StreamWriter("Staff.txt", True)    'This makes sure there is actually a textfile to enter/read data. If not, it creates a new blank one.
+            sw.WriteLine("")
+            sw.Close()
+            MsgBox("A new file has been created", vbExclamation, "Warning!") 'telling the user about the new file creation
+        End If
+
+        Dim StaffData() As String = File.ReadAllLines("Staff.txt") 'retriving the data inside the text file Staff.txt
+        For i = 0 To UBound(StaffData)
+            If Trim(Mid(StaffData(i), 1, 50)) = "" Then 'if the record is empty no calculation is proformed
+            Else
+
+
+                If Trim(Mid(StaffData(i), 1, 50)) >= txtStaffID.Text Then 'if a client id inside the text file is larger than the default or any other client ids in the text file a new highest client id will be set e.g. if the highest client id in the text file is 10003 then this code will set the next client id as the same but incremented by 1
+                    txtStaffID.Text = Trim(Mid(StaffData(i), 1, 50)) + 1
+                End If
+            End If
+        Next i
+
+
+
+    End Sub
+
+    Private Sub btnSave_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSave.Click
+
+
+        If txtDepartment.Text.Replace(" ", "") = "" Then
+            MsgBox("Please enter a Department.")
+        End If
+        If txtFirstName.Text.Replace(" ", "") = "" Then
+            MsgBox("Please enter a First Name.")
+        End If
+        If txtEmergencyNo.Text.Replace(" ", "") = "" Then
+            MsgBox("Please enter a Emergency Telephone Number.")
+        End If
+        If txtHomeAdd.Text.Replace(" ", "") = "" Then
+            MsgBox("Please enter a Home Address.")
+        End If
+        If txtSurname.Text.Replace(" ", "") = "" Then 'checks if there is data in the textbox required.  - presence check
+            MsgBox("Please enter a Surname.")
+        End If
+
+        If dtpDOB.Value.Year > Date.Now.Year Then 'check to make sure the user hasn't entered a birth date in the future. -range check
+            MsgBox("You have entered a date of birth in the furture. Please enter a valid Date of Birth.")
+        End If
+
+        If IsNumeric(txtEmergencyNo.Text.Replace(" ", "")) = False Then 'checks that the user has entered a number and not text or other symbols. -data type check
+            MsgBox("The emergency telephone number you have entered contains forign characters. Please enter a valid Phone Number.")
+        End If
+
+        If txtFirstName.Text.Length > 50 Then 'checks that the user has not entered a field that is too long to be stored in the text file - length check
+            MsgBox("First name entered is too long. Please enter a valid First Name.")
+        End If
+        If txtSurname.Text.Length > 50 Then
+            MsgBox("Surname entered is too long. Please enter a valid Surname.")
+        End If
+        If txtHomeAdd.Text.Length > 50 Then
+            MsgBox("Home Address entered is too long. Please enter a valid Home Address.")
+        End If
+        If txtDepartment.Text.Length > 50 Then
+            MsgBox("Department entered is too long. Please enter a valid Department.")
+        End If
+
+        If txtEmergencyNo.Text.Replace(" ", "").Length > 11 Then
+            MsgBox("The Phone Number you have entered is too long. Please enter a valid Phone Number.")
+        End If
+
+        If txtEmergencyNo.Text.Replace(" ", "").Length < 11 Then
+            MsgBox("The Phone Number you have entered is too short. Please enter a valid Phone Number.")
+        End If
+
+
+
+
+
+
+        Dim CountGot As Integer
+        Dim StaffDatas() As String = File.ReadAllLines("Staff.txt") 'retrives the records inside the text file.
+        For i = 0 To UBound(StaffDatas)
+
+            CountGot = 0
+            If Trim(Mid(StaffDatas(i), 51, 50)) = txtFirstName.Text Then CountGot = CountGot + 1
+            If Trim(Mid(StaffDatas(i), 101, 50)) = txtSurname.Text Then CountGot = CountGot + 1
+            If Trim(Mid(StaffDatas(i), 151, 50)) = txtHomeAdd.Text Then CountGot = CountGot + 1
+            If Trim(Mid(StaffDatas(i), 201, 50)) = txtEmergencyNo.Text Then CountGot = CountGot + 1
+            If Trim(Mid(StaffDatas(i), 251, 50)) = txtDepartment.Text Then CountGot = CountGot + 1
+            If Trim(Mid(StaffDatas(i), 301, 50)) = dtpDOB.Value Then CountGot = CountGot + 1 'makes sure there are no duplicate records by checking the record attempting to be entered against current records in the text file
+            If CountGot = 6 Then
+                MsgBox("Record is already on the system.") 'outputting to the user if there is already an identical record in the text file
+            End If
+        Next i
+
+
+
+
+
+        Dim StaffData As New Staff
+        Dim sw As New System.IO.StreamWriter("Staff.txt", True)
+        StaffData.StaffId = LSet(txtStaffID.Text, 50)
+        StaffData.FirstName = LSet(txtFirstName.Text, 50)
+        StaffData.Surname = LSet(txtSurname.Text, 50)
+        StaffData.HomeAdd = LSet(txtHomeAdd.Text, 50)                      'Filling the structure with data.
+        StaffData.EmergencyNo = LSet(txtEmergencyNo.Text, 50)
+        StaffData.Department = LSet(txtDepartment.Text, 50)
+        StaffData.DOB = LSet(dtpDOB.Value, 50)
+
+        sw.WriteLine(StaffData.StaffId & StaffData.FirstName & StaffData.Surname & StaffData.HomeAdd & StaffData.EmergencyNo) 'using the structure to output the data into a serial text file.
+        sw.Close()                                                                  'Always need to close afterwards
+        MsgBox("File Saved!") 'notifying the user that the record was successfully added to the text file.
+
+        For i = 0 To UBound(StaffDatas)
+            If Trim(Mid(StaffDatas(i), 1, 50)) = "" Then 'if the record is empty no calculation is proformed
+            Else
+
+
+                If Trim(Mid(StaffDatas(i), 1, 50)) >= txtStaffID.Text Then 'if a client id inside the text file is larger than the default or any other client ids in the text file a new highest client id will be set e.g. if the highest client id in the text file is 10003 then this code will set the next client id as the same but incremented by 1
+                    txtStaffID.Text = Trim(Mid(StaffDatas(i), 1, 50)) + 1 'incrementing the client id
+                End If
+            End If
+        Next i
+
+    End Sub
+
+
+    Private Sub btnRetreive_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnRetreive.Click
+
+        txtDepartment.Enabled = False
+        txtEmergencyNo.Enabled = False
+        txtFirstName.Enabled = False
+        txtSurname.Enabled = False
+        txtHomeAdd.Enabled = False
+        dtpDOB.Enabled = False
+        btnBack.Visible = True
+        cmbStaffID.Visible = True
+        btnRetreive.Visible = False 'preparing for a view where the user cannot edit data in the textboxes or date time pickers
+
+
+        Dim StaffData() As String = File.ReadAllLines("Staff.txt") 'retrives the records inside the text file.
+        For i = 0 To UBound(StaffData)
+            cmbStaffID.Items.Add(Trim(Mid(StaffData(i), 1, 50))) 'adding all the clientids assosiated with records that are in the textbox to a combobox.
+        Next i
+
+
+
+
+
+    End Sub
+
+    Private Sub btnBack_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnBack.Click
+
+        txtDepartment.Enabled = True
+        txtEmergencyNo.Enabled = True
+        txtFirstName.Enabled = True
+        txtSurname.Enabled = True
+        txtHomeAdd.Enabled = True
+        dtpDOB.Enabled = True
+        btnBack.Visible = False
+        cmbStaffID.Visible = False
+        btnRetreive.Visible = True
+
+        Dim StaffDatas() As String = File.ReadAllLines("Staff.txt") 'retrives the records inside the text file.
+        For i = 0 To UBound(StaffDatas)
+            If Trim(Mid(StaffDatas(i), 1, 50)) = "" Then 'if the record is empty no calculation is proformed
+            Else
+
+
+                If Trim(Mid(StaffDatas(i), 1, 50)) >= txtStaffID.Text Then 'if a client id inside the text file is larger than the default or any other client ids in the text file a new highest client id will be set e.g. if the highest client id in the text file is 10003 then this code will set the next client id as the same but incremented by 1
+                    txtStaffID.Text = Trim(Mid(StaffDatas(i), 1, 50)) + 1 'incrementing the client id by 1
+                End If
+            End If
+        Next i
+
+    End Sub
+
+    Private Sub cmbStaffID_SelectedIndexChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles cmbStaffID.SelectedIndexChanged
+
+
+
+
+        Dim StaffData() As String = File.ReadAllLines("Staff.txt") 'retrives the records inside the text file.
+        For i = 0 To UBound(StaffData)
+            If Trim(Mid(StaffData(i), 1, 50)) = "" Then 'if the record is empty no calculation is proformed
+            Else
+
+
+                If Trim(Mid(StaffData(i), 1, 50)) = cmbStaffID.Text Then 'if the clientif of the record in question is the same as the search client id then that record is displayed in textboxes and date time pivkers.
+                    txtStaffID.Text = Trim(Mid(StaffData(i), 1, 50))
+                    txtFirstName.Text = Trim(Mid(StaffData(i), 51, 50))
+                    txtSurname.Text = Trim(Mid(StaffData(i), 101, 50))
+                    txtHomeAdd.Text = Trim(Mid(StaffData(i), 151, 50))
+                    txtEmergencyNo.Text = Trim(Mid(StaffData(i), 201, 50))
+                    txtDepartment.Text = Trim(Mid(StaffData(i), 251, 50))
+                    dtpDOB.Text = Trim(Mid(StaffData(i), 301, 50))
+                End If
+            End If
+        Next i
+
+    End Sub
+End Class
